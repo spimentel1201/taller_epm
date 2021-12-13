6600bdd88d3b6f14488c5045fa02bd126bef95f7
Sonny <spimentelm1201@gmail.com>
2021-12-13 08:41:39 -0500

    cambios port listen

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
     res.send('Dashboard is running')
 })*/
 const host = '0.0.0.0';
-const port = process.env.APP_PORT || 3000;
+const port = process.env.APP_PORT || 5000;
 app.listen(port, host, () => {
     console.log('Server running ok on: ', port)
 })
